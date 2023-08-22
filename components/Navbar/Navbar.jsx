4e49d673c6c99164c14185fa9ff1fdd41f17fb83
StephenHenryJr <shenryjr_85@hotmail.com>
2023-08-22 19:10:19 -0400

    added bottom border to links on hover

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { BsSearch } from "react-icons/bs";
+import { BiMenu } from "react-icons/bi";
 
 import { navigationLinks } from "@/constants";
 
 const Navbar = () => {
   return (
     <div className="bg-sky-800 px-32 h-24 flex justify-around items-center">
-      
       {/* LOGO  */}
       <Link href={"/"}>
         <Image src="/ifds-logo.webp" width={300} height={300} alt="IFDS Logo" />
@@ -20,20 +20,22 @@ const Navbar = () => {
       {/* LINKS */}
       <div className="2xl:flex hidden h-full">
         {navigationLinks.map((link) => (
-          <div className="group relative text-white cursor-pointer px-6 h-full flex flex-col items-center justify-center" key={link.title}>
+          <div
+            className="group relative text-white cursor-pointer px-6 h-full flex flex-col items-center justify-center"
+            key={link.title}
+          >
             <div className="group-hover:bg-white w-2 h-2 rounded-full mb-2" />
             {link.title}
 
             {link.links && (
               <div className="group-hover:block hidden bg-sky-800 absolute w-max top-[91px] p-4 border-t-4 rounded-b-md">
                 {link.links.map((item) => (
-                  <li key={item} className="my-1">
+                  <li key={item} className="my-1 hover:text-gray-300">
                     {item.title}
                   </li>
                 ))}
               </div>
             )}
-
           </div>
         ))}
       </div>
@@ -48,6 +50,11 @@ const Navbar = () => {
             placeholder="Search"
           />
         </form>
+      </div>
+
+      {/* MENU ICON */}
+      <div className="absolute left-10">
+        <BiMenu className="text-white cursor-pointer" size={35}/>
       </div>
     </div>
   );
